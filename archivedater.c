40c46616497cb1b0dc14ed89efe4e62803a5d5d7
iliaha2005@gmail.com <iliaha2005@gmail.com>
2024-04-24 18:58:19 +0300

    feat: several option flags were added

--- archivedater.c
+++ archivedater.c
@@ -30,6 +30,12 @@ LogLevel current_log_level = INFO;
 
 typedef enum {
     OPT_VERBOSE,
+    OPT_QUIET,
+    OPT_LOGLEVEL,
+    OPT_OUTPUTDIR,
+    OPT_HELP,
+    OPT_VERSION,
+    OPT_DRYRUN,
     OPT_UNKNOWN
 } Option;
 
@@ -122,12 +128,25 @@ ParsedOption parse_option(const char *arg) {
 
     if (strcmp(arg, "-v") == 0) {
         result.option = OPT_VERBOSE;
+    } else if (strcmp(arg, "-q") == 0) {
+        result.option = OPT_QUIET;
+    } else if (strcmp(arg, "-l") == 0) {
+        result.option = OPT_LOGLEVEL;
+    } else if (strcmp(arg, "-o") == 0) {
+        result.option = OPT_OUTPUTDIR;
+    } else if (strcmp(arg, "-h") == 0) {
+        result.option = OPT_HELP;
+    } else if (strcmp(arg, "--version") == 0) {
+        result.option = OPT_VERSION;
+    } else if (strcmp(arg, "-d") == 0) {
+        result.option = OPT_DRYRUN;
     } else {
         result.option = OPT_UNKNOWN;
     }
 
     return result;
 }
+
 
 // Function for parsing command-line arguments
 void parse_command_line_arguments(int argc, char *argv[]) {
@@ -136,9 +155,53 @@ void parse_command_line_arguments(int argc, char *argv[]) {
 
         switch (option.option) {
             case OPT_VERBOSE:
-                // Enable verbose logging
                 current_log_level = DEBUG;
                 log_message(DEBUG, "Verbose mode enabled");
+                break;
+            case OPT_QUIET:
+                current_log_level = ERROR;
+                log_message(INFO, "Quiet mode enabled");
+                break;
+            case OPT_LOGLEVEL:
+                if (i + 1 < argc) {
+                    i++;
+                    if (strcmp(argv[i], "DEBUG") == 0) {
+                        current_log_level = DEBUG;
+                    } else if (strcmp(argv[i], "INFO") == 0) {
+                        current_log_level = INFO;
+                    } else if (strcmp(argv[i], "WARN") == 0) {
+                        current_log_level = WARN;
+                    } else if (strcmp(argv[i], "ERROR") == 0) {
+                        current_log_level = ERROR;
+                    } else {
+                        log_message(ERROR, "Invalid log level '%s'", argv[i]);
+                        exit(1);
+                    }
+                } else {
+                    log_message(ERROR, "Missing argument for -l");
+                    exit(1);
+                }
+                break;
+            case OPT_OUTPUTDIR:
+                if (i + 1 < argc) {
+                    i++;
+                    if (chdir(argv[i]) != 0) {
+                        perror("chdir() to specified output directory failed");
+                        exit(1);
+                    }
+                    log_message(INFO, "Changed working directory to %s", argv[i]);
+                } else {
+                    log_message(ERROR, "Missing argument for -o");
+                    exit(1);
+                }
+                break;
+            case OPT_HELP:
+                print_help_message();
+                exit(0);
+                break;
+            case OPT_VERSION:
+                printf("archivedater version 1.0\n");
+                exit(0);
                 break;
             case OPT_UNKNOWN:
                 if(argv[i][0] == '-') {
@@ -148,6 +211,41 @@ void parse_command_line_arguments(int argc, char *argv[]) {
                 break;
         }
     }
+}
+
+// Function for printing help message
+void print_help_message() {
+    printf("NAME\n");
+    printf("    archivedater - Corrects dates within archives, prioritizing the most relevant file types\n");
+    printf("\n");
+    printf("SYNOPSIS\n");
+    printf("    archivedater [FILE_TYPE] [ARCHIVE_PATH] [OPTIONS]\n");
+    printf("\n");
+    printf("DESCRIPTION\n");
+    printf("    This utility is designed to correct dates within archives, prioritizing the most relevant file types specified by the user. It aims to enhance the accuracy and consistency of date information across various archived files, providing a streamlined approach to archive management. By allowing users to select the types of files to be considered for date correction, this utility offers a flexible solution tailored to specific archive needs.\n");
+    printf("\n");
+    printf("OPTIONS\n");
+    printf("    -h, --help\n");
+    printf("        Display this help message and exit.\n");
+    printf("    -v, --verbose\n");
+    printf("        Enable verbose mode.\n");
+    printf("    -q, --quiet\n");
+    printf("        Enable quiet mode.\n");
+    printf("    -l, --loglevel\n");
+    printf("        Set log level (DEBUG, INFO, WARN, ERROR).\n");
+    printf("    -o, --outputdir\n");
+    printf("        Set output directory.\n");
+    printf("    --version\n");
+    printf("        Display version information and exit.\n");
+    printf("    -d, --dryrun\n");
+    printf("        Perform a dry run without making changes.\n");
+    printf("\n");
+    printf("EXIT STATUS\n");
+    printf("    0 Successful execution.\n");
+    printf("    1 An error occurred.\n");
+    printf("\n");
+    printf("SEE ALSO\n");
+    printf("    man(1), man-pages(7)\n");
 }
 
 // Function for processing the archive
@@ -182,7 +280,6 @@ void log_message(LogLevel level, const char *format, ...) {
     strftime(timestamp, sizeof(timestamp), "%Y-%m-%d %H:%M:%S", timeinfo);
 
     // Log to console
-    usleep(500000);
     printf("%s %s: ", timestamp, level == DEBUG ? "DEBUG" : level == INFO ? "INFO" : level == WARN ? "WARN" : "ERROR");
     vprintf(format, args);
     printf("\n");
